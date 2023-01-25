4e432b417b9c1fd3dc4a0dda7271cff83480fe47
Christa Zoufal <ouf@zurich.ibm.com>
2023-01-25 18:36:26 +0100

    initial commit

---
+++ machine_learning/README.md
@@ -0,0 +1,51 @@
+# Benchmarking quantum machine learning for finance
+
+This subdirectory holds the machine learning track of the benchmarks of quantum computing for financial services.
+
+## Problem definitions
+
+To benchmark quantum solvers for quantum machine learning problems in a financial setting we consider finance oriented benchmarks and generic benchmarks.
+The financial benchmarks include
+
+* Fraud Detection
+* Trading
+* Customer Analysis
+* Feature selection
+
+The benchmarks are based on the following quantum algorithms
+
+* [Variational quantum classifier (VQC)](https://github.com/Qiskit/qiskit-machine-learning/blob/main/qiskit_machine_learning/algorithms/classifiers/vqc.py) 
+* [Quantum support vector machine classifier (QSVC)](https://github.com/Qiskit/qiskit-machine-learning/blob/main/qiskit_machine_learning/algorithms/classifiers/qsvc.py)
+* [Neural network classifier](https://github.com/Qiskit/qiskit-machine-learning/blob/main/qiskit_machine_learning/algorithms/classifiers/neural_network_classifier.py)
+* Black-box binary optimizations (BBBO)
+
+## Metrics  
+
+For each of the above mentioned algorithms this repository provides the tools to benchmark these problems with respect to the following metrics.
+
+* **Performance** is measured with respect to standard ML metrics such as recall, accuracy and precision.
+
+* **Resource requirements** is measured considering various aspects: the training time, the inference time of a model, and the amount of data required to train a model up to a pre-defined accuracy. to obtain a solution.
+  For the temporal metrics, it should be noted that this includes the classical pre-processing time it takes to prepares the quantum circuits as well as the execution of the quantum circuits - except for queuing time.
+  
+* **Intrinsic model properties** This class of metrics includes generalization bounds and capacity measures.
+
+## Datasets
+
+To support comparability, we provide a set of datasets which are relevant for the benchmarking problems given above.
+Notably, some of the references below provide links to publicly available online resources and other link to files which generate a benchmarking dataset.
+
+* [Publicly available credit risk dataset](online:http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data)
+* [Fraud detection dataset - talk to Erik Altman]()
+* [GARCH data - file to be written]()
+
+## Result summary
+
+This section summarizes some results of the benchmarks. 
+The table below has the key numbers but more importantly points to the file that runs the benchmark.
+
+| Algorithm | Problem           | Dataset                 | Metric | Result | File |
+|-----------|-------------------|-------------------------|--------|--------|------|
+| VQC       | Fraud detection   | German Credit Risk Data |        |        |      |
+| BBBO      | Feature Selection |                         |        |        |      |
+
---
+++ machine_learning/datasets/load_credit_risk_data.py
@@ -0,0 +1,86 @@
+import sys
+
+import pandas as pd
+from io import StringIO
+
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+
+
+def data_loading(seed=77):
+    ###
+    # Function to load credit risk data and return a trian test split
+    ###
+    if sys.version_info[0] == 3:
+        from urllib.request import urlopen
+    else:
+        from urllib import urlopen
+    with urlopen("http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/"
+                 "german/german.data") as url:
+        byte_data = url.read()
+
+    str_data = StringIO(str(byte_data,'utf-8'))
+
+    columns = ['Existing_CheckingAccount_Status',
+            'Duration_In_Month',
+              'Credit_History',
+              'Loan_Purpose',
+              'Credit_Amount',
+              'Saving_Account_Or_Bonds',
+              'Present_Employment_Since',
+              'Installment_Rate_Pct_Disposable_Income',
+              'Marital_Status_And_Gender',
+              'Other_Debtors_Or_Guarantors',
+              'Current_Residence_Duration',
+              'Property',
+              'Age_In_Years',
+              'Other_Installment_Plans',
+              'Housing_Status',
+              'Number_Existing_Credits_At_Bank',
+              'Employment_Status',
+              'Number_People_Liable_Maintenance',
+              'Telephone_Registration_Status',
+              'Foreign_Worker',
+              'Credit_Risk_Status']
+
+    df = pd.read_csv(str_data,
+                     sep=" ",
+                     header=None,
+                     names=columns,
+                     dtype={'Existing_CheckingAccount_Status': str,
+                             'Duration_In_Month': int,
+                             'Credit_History': str,
+                             'Loan_Purpose': str,
+                             'Credit_Amount': int,
+                             'Saving_Account_Or_Bonds': str,
+                             'Present_Employment_Since': str,
+                             'Installment_Rate_Pct_Disposable_Income': int,
+                             'Marital_Status_And_Gender': str,
+                             'Other_Debtors_Or_Guarantors': str,
+                             'Current_Residence_Duration': int,
+                             'Property': str,
+                             'Age_In_Years': int,
+                             'Other_Installment_Plans': str,
+                             'Housing_Status': str,
+                             'Number_Existing_Credits_At_Bank': int,
+                             'Employment_Status': str,
+                             'Number_People_Liable_Maintenance': int,
+                             'Telephone_Registration_Status': str,
+                             'Foreign_Worker': str,
+                             'Credit_Risk_Status': str})
+    X, y = df.drop('Credit_Risk_Status', axis=1), pd.DataFrame(LabelEncoder().fit_transform(df['Credit_Risk_Status']),
+                    columns=["is_credit_risky"])
+    categorical_X = pd.get_dummies(X.select_dtypes('object'))
+    numerical_X = pd.DataFrame(StandardScaler().fit_transform(X.select_dtypes('int')),
+                               columns=["Duration_In_Month_zscl",
+                                        "Credit_Amount_zscl",
+                                        "Installment_Rate_Pct_Disposable_Income_zscl",
+                                        "Current_Residence_Duration_zscl",
+                                        "Age_In_Years_zscl",
+                                        "Number_Existing_Credits_At_Bank_zscl",
+                                        "Number_People_Liable_Maintenance_zscl"])
+    concatenated_X = pd.concat([categorical_X, numerical_X], axis=1)
+    X_Y = pd.concat([concatenated_X, y], axis=1)
+    train, test = train_test_split(X_Y, test_size=0.3, stratify=X_Y['is_credit_risky'],
+                                   random_state=seed)
+    return (train, test)
